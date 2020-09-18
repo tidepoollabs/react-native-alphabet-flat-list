acbc8e7513d34cd970edf89baff0e92a00c6c753
arman <griarman>
2020-09-18 15:08:10 +0400

    chore(Build): build typescript

---
+++ build/SectionListItem.d.ts
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+interface IProps {
+    title: string;
+    height: number;
+    active: boolean;
+    alphabetTextViewStyles: object;
+}
+declare const SectionListItem: FC<IProps>;
+export default SectionListItem;
---
+++ build/SectionListItem.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = require("react");
+const react_native_1 = require("react-native");
+const styles_1 = require("./styles");
+const SectionListItem = function (props) {
+    return (<react_native_1.View style={[styles_1.default.sectionListItemContainer, { height: props.height }]}>
+      <react_native_1.View style={[styles_1.default.sectionListItemWrapper, {
+            backgroundColor: 'transparent',
+        }]}>
+        <react_native_1.Text style={[styles_1.default.sectionListItemText, {
+            color: props.active ? 'white' : '#333',
+        }, props.alphabetTextViewStyles]}>
+          {props.title}
+        </react_native_1.Text>
+      </react_native_1.View>
+    </react_native_1.View>);
+};
+exports.default = SectionListItem;
+//# sourceMappingURL=SectionListItem.js.map
